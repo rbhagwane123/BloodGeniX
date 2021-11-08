2ebfcf455e03b76d6eb55f0a8e1c4a8e4b6104ca
Rupesh <rbhagwane123@gmail.com>
2021-11-08 13:33:31 +0530

    This is Blood Donation and Receiving Apk devolped using android and firebase.

--- app/src/main/java/com/example/bloodgenix/Signup_Screen_1.java
+++ app/src/main/java/com/example/bloodgenix/Signup_Screen_1.java
@@ -1,19 +1,38 @@
 package com.example.bloodgenix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.drjacky.imagepicker.ImagePicker;
 
 public class Signup_Screen_1 extends AppCompatActivity {
 
-    ImageButton nextButton_1;
+    ImageButton nextButton_1, profilePickerBtn;
+    ImageView profileImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen1);
+
+        profileImage = findViewById(R.id.profileImage);
+        profilePickerBtn = findViewById(R.id.profilePickerBtn);
+        profilePickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.Companion.with(Signup_Screen_1.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .cropOval()	    		//Allow dimmed layer to have a circle inside
+                        .maxResultSize(144,144)
+                        .start();
+            }
+        });
 
         nextButton_1 = findViewById(R.id.nextButton_1);
         nextButton_1.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +42,12 @@ public class Signup_Screen_1 extends AppCompatActivity {
                 startActivity(signup_2);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        profileImage.setImageURI(uri);
     }
 }
--- app/src/main/java/com/example/bloodgenix/Signup_Screen_2.java
+++ app/src/main/java/com/example/bloodgenix/Signup_Screen_2.java
@@ -1,5 +1,8 @@
 package com.example.bloodgenix;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,17 +14,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Signup_Screen_2 extends AppCompatActivity {
 
     private AutoCompleteTextView gender;
     ArrayAdapter <String> arrayAdapter;
     DatePicker datePicker;
+    ImageButton docSelection;
     ImageButton nextButton_2;
+    TextInputEditText docText;
+    TextInputLayout docLayout;
+    int Requestcode =1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen2);
 
+//        for gender selection code
         gender = findViewById(R.id.Gender);
         String [] gender_val= getResources().getStringArray(R.array.gender);
         arrayAdapter= new ArrayAdapter<String>(this,R.layout.dropdown_item_gender,gender_val);
@@ -30,11 +43,27 @@ public class Signup_Screen_2 extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
-                Toast.makeText(Signup_Screen_2.this, item, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(Signup_Screen_2.this, item, Toast.LENGTH_SHORT).show();
+            }
+        });
+        Context context = getApplicationContext();
+        datePicker = findViewById(R.id.Dob);
+
+//        file Uploading code
+        docText = findViewById(R.id.docText);
+        docLayout = findViewById(R.id.docLayout);
+
+        docSelection = findViewById(R.id.docSelection);
+        docSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("*/*");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,Requestcode);
             }
         });
 
-        datePicker = findViewById(R.id.Dob);
         nextButton_2 = findViewById(R.id.nextButton_2);
         nextButton_2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +71,26 @@ public class Signup_Screen_2 extends AppCompatActivity {
                 int day = datePicker.getDayOfMonth();
                 int month = datePicker.getMonth();
                 int year = datePicker.getYear();
-                Toast.makeText(Signup_Screen_2.this, day+" "+month+" "+year, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(Signup_Screen_2.this, day+" "+month+" "+year, Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+//        Toast.makeText(Signup_Screen_2.this, requestCode+" "+data.getData(), Toast.LENGTH_SHORT).show();
+        if (requestCode ==1){
+            if (data == null){
+                return;
+            }
+            docLayout.setVisibility(View.VISIBLE);
+            Uri docUri = data.getData();
+            docText.setText(data.getDataString().substring(data.getDataString().lastIndexOf("/")+1)+".pdf");
+        }
+        else {
+            Toast.makeText(Signup_Screen_2.this, "Please try again ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
